2e1cf3f055328d849a05c353469d713f4be48aa2
Danilo Aleixo <danilo_aleixo@hotmail.com>
2018-01-27 10:56:03 -0200

    Coloquei para selecionar uma opção

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,16 @@
 import { Component } from '@angular/core';
 
 
-export interface SearchElement {
+export interface ISearchElement {
   tokens: string[];
-  value: string;
+  title: string;
+  category: string;
+  city: string;
+  telephone: string;
+  email?: string;
+  whatsapp?: string;
+  URL?: string;
+  photoURL?: string;
 }
 
 @Component({
@@ -13,26 +20,76 @@ export interface SearchElement {
 })
 export class AppComponent {
 
-	public results: SearchElement[] = 
-	[
-	  {
-	    value: 'Era uma vez',
-	    tokens: ['Era', 'uma', 'vez']
-	  },
-	  {
-	    value: 'Uma historia muito bonita',
-	    tokens: ['Uma', 'historia', 'muito', 'bonita']
-	  }
-	];
+	public results: ISearchElement[] = [];
 
-  	title = 'app';
-
+	// Component state variables
+	public showedResults: ISearchElement[] = [];
   	public inputText: string = '';
+  	public resultSelected: ISearchElement = null;
 
-  	constructor() {}
+  	constructor() {
+  		this.results = this.constructMockResults();
+  		this.showedResults = this.results;
+  	}
 
   	public handleInputChange(event) {
+  		this.showedResults = this.filterResults(this.results, this.inputText.toLowerCase());
+  		this.resultSelected = null;
+  	}
 
+
+  	public selectResult(result: ISearchElement) {
+  		this.resultSelected = result;
+  	}
+
+  	private constructMockResults(): ISearchElement[] {
+  		const results: ISearchElement[] = [];
+
+  		const categories = ['Autocadista', 'Academia', 'Pizzaria', 'Borracharia'];
+
+  		// Insert info
+  		for(let i = 0; i < 10; i++) {
+  			let result: ISearchElement = {
+  				category: categories[parseInt((Math.random() * categories.length).toString())],
+  				city: 'Diadema',
+  				title: 'Empresa ' + i,
+  				telephone: '11 994349394',
+  				tokens: [] 
+  			};
+
+  			results.push(result);
+  		}
+
+  		// Append tokens
+  		results.map((result: ISearchElement) => {
+  			result.tokens = this.makeTokens(result);
+  		});
+
+
+  		return results;
+  	}
+
+  	private makeTokens(element: ISearchElement): string[] {
+  		const result: string[] = [];
+
+  		Object.keys(element).map((key: string) => {
+  			if(key != 'tokens') {
+	  			let value: string = element[key];
+	  			value.split(' ').map((s: string) => {
+	  				result.push(s.toLowerCase());
+	  			});
+  			}
+  		});
+
+  		return result;
+  	}
+
+  	private filterResults(allResults: ISearchElement[], value: string): ISearchElement[] {
+  		return allResults.filter((result: ISearchElement) => {
+  			return result.tokens
+  			.filter((token: string) => token.includes(value))
+  			.length > 0;
+  		});
   	}
 
 }
